abad94e63ae451fa017b226663b47d07a1832959
szensk <eleinvisible@gmail.com>
2013-12-20 11:15:30 -0600

    cleaned up Love API completions

---
+++ completions.py
@@ -0,0 +1,13 @@
+#completions.py
+import sublime
+import sublime_plugin
+import re
+
+class LoveCompletions(sublime_plugin.EventListener):
+    ST = 3000 if sublime.version() == '' else int(sublime.version())
+
+    def on_query_completions(self, view, prefix, locations):
+        if self.ST < 3000 and ("moonscript" in view.scope_name(locations[0])):
+            seps = view.settings().get("word_separators")
+            seps = seps.replace('.', '')
+            view.settings().set("word_separators", seps)
